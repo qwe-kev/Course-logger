6561b7af0685830af80bf4a13724f20634f4d1e7
qwe-kev <kevray39@gmail.com>
2023-07-23 17:24:57 +0530

    setup class based styles

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    document.getElementsByClassName("button")[0].classList.remove("invalid");
+    setIsValid(true);
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+
+      document.getElementsByClassName("button")[0].classList.add("invalid");
+
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? "invalid" : " "}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
